f54a49c9e5a93b348181a1713e4c2af3315d99cc
Carlos-Passarelli <carloseduardopassarelli@gmail.com>
2026-06-20 10:43:48 -0300

    Projeto Desenvolvido no Android Studio

---
+++ AplicativoCarros/app/src/main/java/com/example/aplicativocarros/MainActivity4.java
@@ -0,0 +1,50 @@
+package com.example.aplicativocarros;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity4 extends AppCompatActivity {
+
+    Button btnOuvir, btnVoltar;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main4);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnOuvir = findViewById(R.id.btn_ouvir);
+        btnVoltar = findViewById(R.id.btn_voltar);
+
+        btnOuvir.setOnClickListener(view -> tocarSom(R.raw.amg_g63));
+        btnVoltar.setOnClickListener(view -> finish());
+    }
+
+    private void tocarSom(int som) {
+        if (mediaPlayer != null) mediaPlayer.release();
+
+        mediaPlayer = MediaPlayer.create(this, som);
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) mediaPlayer.release();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AplicativoCarros
 Aplicativo mobile de carros usando JAVA
+
+* Desenvolvido durante o minicurso de introdução à programação mobile da Fábrica de Software - UNISAGRADO
